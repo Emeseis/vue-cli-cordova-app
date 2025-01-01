a7613c98cb11dd7dfc8b7c69cd489064e64030f1
Maycon Negrini <maycon128@hotmail.com>
2024-12-31 22:25:30 -0300

    ajust routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: () => import('../views/About.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.name
+  next()
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home">
+    <h1>This is the homepage</h1>
     <v-img height="200" src="../assets/logo.png"></v-img>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home"
 }
 </script>
